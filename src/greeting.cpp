3eaed8f518ab1eaae064b5f01e332f20799793d3
Igor Ermakov <ermakov.i.o@yandex.ru>
2020-02-13 00:45:50 +0300

    WTF?

--- src/greeting.cpp
+++ src/greeting.cpp
@@ -3,6 +3,12 @@
 #include <thread>
 #include <iostream>
 
+#ifdef POTATO_BULBA_EXPORT_SYMBOL
+#error 1
+#else
+#error 2
+#endif
+
 namespace bulba {
 
 POTATO_BULBA_EXPORT void greet(std::string name)
